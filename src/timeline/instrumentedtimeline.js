d343e5de4b1489fb31e5c2c37f9f34ce22b96233
Peter Rank <peter.rank@mobilexag.de>
2021-01-26 18:43:59 +0100

    fix closeJumpTo-Bug

--- src/timeline/instrumentedtimeline.js
+++ src/timeline/instrumentedtimeline.js
@@ -195,7 +195,6 @@ class InstrumentedTimeline extends React.Component {
         if (this.props.onLongPress) {
             this.props.onLongPress(timelineevent);
         }
-        this.refs.nowbutton.closeJumpTo();
     }
 
     onTimelineClick(timelineevent) {
